b39c8c7d06c3ee71e68b91241fc0f7924aff5377
ttsukagoshi <55706659+ttsukagoshi@users.noreply.github.com>
2020-12-16 22:34:43 +0900

    Declare class 'chrome' as global

--- src/background.js
+++ src/background.js
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/* global chrome */
+
 chrome.runtime.onInstalled.addListener(function () {
     chrome.contextMenus.create({
         'id': 'ImgSrc-er',
